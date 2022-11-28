f7b9b05c5e931b39180d3fc2b6342d78d98b7cc5
Bohdan Bordiuh <b.bordiuh@sana-commerce.com>
2022-11-28 22:15:31 +0200

    add test version of main page

--- js/app.js
+++ js/app.js
@@ -3,15 +3,21 @@ let common = {
 		common.navigation();
 	},
 	navigation: function() {
+		let bLazy = new Blazy({});
+
 		const menuOpenButton = document.querySelector('.menu-open');
 		const menuCloseButton = document.querySelector('.menu-close');
 		const body = document.querySelector('body');
+		let subButton = document.querySelector('.sub-form button');
+		let subForm = document.querySelector('.sub');
 
 		menuOpenButton.addEventListener('click', () => body.classList.add('menu-open'));
 		menuCloseButton.addEventListener('click', () => body.classList.remove('menu-open'));
-		
-		let bLazy = new Blazy({});
-		
+		subButton.addEventListener('click', () => subForm.classList.add('edit'));
+		subForm.addEventListener('submit', e => {
+			e.preventDefault();
+			subForm.classList.add('success');
+		});
 		
 		// window.onscroll = function() {fixedHeader()};
 		// let header = document.querySelector(".header");
