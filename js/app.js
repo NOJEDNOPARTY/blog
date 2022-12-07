8ed79d2edca6697a9cabfca43d52d7b3d7e0a93c
Bohdan Bordiuh <b.bordiuh@sana-commerce.com>
2022-12-07 16:06:41 +0200

    add comments block without validation

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ let common = {
 	init: function() {
 		common.navigation();
 		common.slider();
+		common.form();
 	},
 	navigation: function() {
 		const bLazy = new Blazy({});
@@ -10,21 +11,12 @@ let common = {
 		const menuOpenButton = document.querySelector('.menu-open');
 		const menuCloseButton = document.querySelector('.menu-close');
 		const body = document.querySelector('body');
-		let subButton = document.querySelector('.sub-form button');
-		let subForm = document.querySelector('.sub');
 
 		menuOpenButton.addEventListener('click', () => body.classList.add('menu-open'));
 
 		menuCloseButton.addEventListener('click', () => body.classList.remove('menu-open'));
 
 		window.addEventListener('click', e => !menu.contains(e.target) && !menuOpenButton.contains(e.target) && body.classList.remove('menu-open'));
-
-		subButton.addEventListener('click', () => subForm.classList.add('edit'));
-
-		subForm.addEventListener('submit', e => {
-			e.preventDefault();
-			subForm.classList.add('success');
-		});
 	},
 	slider: function() {
 		let galleryItems = document.querySelectorAll('.gallery-item');
@@ -61,6 +53,85 @@ let common = {
 		splide.mount();
     splide.on('moved', newIndex => galleryCountHandler(newIndex + 1));
 	},
+	form: function() {
+		let subButton = document.querySelector('.sub-form button');
+		let subForm = document.querySelector('.sub');
+
+		subButton.addEventListener('click', () => subForm.classList.add('edit'));
+		subForm.addEventListener('submit', e => {
+			e.preventDefault();
+			subForm.classList.add('success');
+		});
+
+// 		1. Переключатели для ответов
+// 2. Переключатель показать больше комментов
+// 3. Форма отправки при не валидном, для текстареа
+
+		let commentsTextarea = document.querySelectorAll('.comments-form .form-field textarea');
+		let commentsTextareaValue = document.querySelectorAll('.comments-form .form-field textarea').value;
+
+		commentsTextarea.forEach(textarea => textarea.addEventListener('keyup', () => {
+			const textareaLabel = textarea.closest('label');
+			const textareaTextLength = textareaLabel.querySelector('span');
+			const value = textarea.value.length;
+
+			textareaTextLength.innerHTML = 50 - value;
+		}));
+
+		document.querySelectorAll('.show-more-replies').forEach(btn => btn.addEventListener('click', e => {
+			e.preventDefault();
+
+			const textShow = btn.getAttribute('data-show');
+			const textHide = btn.getAttribute('data-hide');
+			const commentsItem = btn.closest('.comments-item');
+			const btnName = btn.querySelector('.show-more-replies-name');
+
+			if(commentsItem.classList.contains('show-replies')) {
+				commentsItem.classList.remove('show-replies');
+				btnName.innerHTML = textShow;
+			}else {
+				commentsItem.classList.add('show-replies');
+				btnName.innerHTML = textHide;
+			}
+		}));
+
+		document.querySelectorAll('.show-more-comments').forEach(btn => btn.addEventListener('click', e => {
+			e.preventDefault();
+
+			const textShow = btn.getAttribute('data-show');
+			const textHide = btn.getAttribute('data-hide');
+			const commentsList = btn.closest('.comments');
+			const btnName = btn.querySelector('span');
+
+			console.lo
+
+			if(commentsList.classList.contains('show-all')) {
+				commentsList.classList.remove('show-all');
+				btnName.innerHTML = textShow;
+			}else {
+				commentsList.classList.add('show-all');
+				btnName.innerHTML = textHide;
+			}
+		}));
+
+		document.querySelectorAll('.comments-form-trigger').forEach(btn => btn.addEventListener('click', e => {
+			e.preventDefault();
+
+			const commentsItemWrap = btn.closest('.comments-item-wrap');
+
+			if(!commentsItemWrap.classList.contains('show'))
+				commentsItemWrap.classList.add('show');
+		}));
+
+		document.querySelectorAll('.hide-form').forEach(btn => btn.addEventListener('click', e => {
+			e.preventDefault();
+
+			const commentsItemWrap = btn.closest('.comments-item-wrap');
+
+			if(commentsItemWrap.classList.contains('show'))
+				commentsItemWrap.classList.remove('show');
+		}));
+	}
 };
 
 (function() {
